873e9451f6c4759237389551c31d6c932909757e
zam007 <zam254227751@gmail.com>
2017-09-05 14:18:17 +0800

    make_archive

---
+++ python3_cook/十三、脚本编程与系统管理/13.8创建和解压压缩文件.py
@@ -0,0 +1,10 @@
+import shutil
+
+# 解压文件Python-3.3.0.tgz
+shutil.unpack_archive('Python-3.3.0.tgz')
+
+# 用zip压缩文件Python-3.3.0.tgz,名字为py33.zip
+shutil.make_archive('py33','zip','Python-3.3.0')
+
+# 可以压缩的模式[('bztar', "bzip2'ed tar-file"), ('gztar', "gzip'ed tar-file"),('tar', 'uncompressed tar file'), ('zip', 'ZIP file')]
+shutil.get_archive_formats()
