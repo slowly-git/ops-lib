23ae841ab54aa6aabd9936fd0afe706af7a163e9
Zhu anming <zam254227751@gmail.com>
2018-01-12 14:20:59 +0800

    ad tmple

---
+++ kubernetes/tmple-pingup-yaml/keepalived_lvs/config_vip.sh
@@ -0,0 +1,5 @@
+ifconfig lo:0 10.1.12.80 netmask 255.255.255.255 broadcast 10.1.12.80
+echo "1" >/proc/sys/net/ipv4/conf/lo/arp_ignore
+echo "2" >/proc/sys/net/ipv4/conf/lo/arp_announce
+echo "1" >/proc/sys/net/ipv4/conf/all/arp_ignore
+echo "2" >/proc/sys/net/ipv4/conf/all/arp_announce
