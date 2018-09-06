8502b601d74f2ebd38cbbb23c4a8542072ed5b14
zhuanming <anming.zhu@tiejin.me>
2018-09-06 18:36:47 +0800

    del & insrt

--- go-ops-cook/go-lib/structtools.go
+++ go-ops-cook/go-lib/structtools.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 	"math/rand"
+	"reflect"
 )
 
 type SpotPrice struct {
@@ -57,7 +57,35 @@ func insertHead(p *Student) {
 		p = stu
 	}
 
+	delNode(p)
 	trans(p)
+}
+
+/*链表：删除一个节点,把上一个节点的next指向下一个节点(此处不考虑头节点问题)*/
+func delNode(p *Student) {
+	//保存上一个节点
+	var prev = p
+	for p != nil {
+		if p.name == "stu6" {
+			prev.next = p.next
+			break
+		}
+		//每次遍历，节点移动
+		prev = p
+		p = p.next
+	}
+}
+
+/*链表：插入一个节点*/
+func insrtNode(p, newNode *Student) {
+	for p != nil {
+		if p.name == "stu6" {
+			newNode.next = p.next
+			p.next = newNode
+			break
+		}
+		p=p.next
+	}
 }
 
 func main() {
