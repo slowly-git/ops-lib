908e060ecf564c505d967a836c609e1efbb4e0c4
zhuanming <anming.zhu@tiejin.me>
2018-09-20 17:35:33 +0800

    add io.EOF

--- go-ops-cook/go-lib/file.go
+++ go-ops-cook/go-lib/file.go
@@ -14,26 +14,26 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
+	fmt.Fprint(f, "line 1\nline 2\nline3\n")
 
-	s := "test"
-
-	fmt.Fprintf(f, "测试写文件 %s\n", s)
-
+	//准备读文件
 	f2, err := os.Open("/tmp/test.log")
 	defer f2.Close()
 	if err != nil {
-		fmt.Println(err)
+		os.Exit(1)
 	}
 	//用bufio读文件
 	s2 := bufio.NewReader(f2)
 	//读到\n结束
-	str2,err := s2.ReadString('\n')
-	if err ==io.EOF{
-		fmt.Println("已经读到文件结尾")
+	for {
+		str2, err := s2.ReadString('\n')
+		if err == io.EOF {
+			fmt.Println("已经读到文件结尾")
+			break
+		}
+		if err != nil {
+			break
+		}
+		fmt.Println(str2)
 	}
-	if err != nil{
-		fmt.Println(err)
-	}
-
-	fmt.Println(str2)
 }
