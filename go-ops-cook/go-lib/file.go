05265c3ac995ec7d84e9471c5bfc2de679dca329
zhuanming <anming.zhu@tiejin.me>
2018-09-21 11:48:56 +0800

    io.copy

--- go-ops-cook/go-lib/file.go
+++ go-ops-cook/go-lib/file.go
@@ -44,4 +44,24 @@ func main() {
 		}
 		fmt.Println(str2)
 	}
+
+	//拷贝文件
+	CopyFile("/tmp/dst_test.log", "/tmp/test.log")
+	fmt.Println("Copy done!")
+}
+
+func CopyFile(dstName, srcName string) (written int64, err error) {
+	src, err := os.Open(srcName)
+	if err != nil {
+		return
+	}
+	defer src.Close()
+
+	dst, err := os.OpenFile(dstName, os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		return
+	}
+	defer dst.Close()
+
+	return io.Copy(dst, src)
 }
